ebb617f923fd332f93422b223aac0b157b050383
Johan Ahlstrand <042ahlstrand@gmail.com>
2019-10-02 19:50:26 +0200

    Added PRAW reddit python wrapper

--- app.py
+++ app.py
@@ -1,4 +1,24 @@
 import json
+import praw
+
+# Create Reddit Object with credentials from external file. See rename keystemplate to keys.json and fill in reddit api credentials.
+def create_reddit_object ():
+    f = open('keys.json', 'r')
+    my_dict = json.load(f)
+    #print(my_dict["client_id"])
+    #print(my_dict["client_secret"])
+    #print(my_dict["user_agent"])
+    create_reddit_object.reddit = praw.Reddit(   client_id= my_dict["client_id"],
+                            client_secret= my_dict["client_secret"],
+                            user_agent= my_dict["user_agent"])
+
+# Parse Reddit submissions, these will be used as test data for N
+def summon_submission ():
+    for submission in create_reddit_object.reddit.subreddit('all').hot(limit=25):
+        print(submission.title)
+        print(submission.id)
+    
+
 
 # Function to write a dict as json to file.json
 def write_json_file ( pet_name ) :
@@ -19,16 +39,20 @@ def read_json_file ():
     print(my_dict)
 
 # Choice of reading or writing to the JSON-file
-val = input("Read(r) or write(w)?: ") 
-if val == "r" :
-    print("Let's read the json data.")
-    read_json_file ()
-    "Thanks for using the pet database."
-if val == "w" :
-    print("Let's write the json data.")
-    mypet = input("Name of pet: ") 
-    write_json_file ( mypet )
-    "Adding that name."
-    "Thanks for using the pet database."
-else :
-    print("Please choose read(r) or write(w). Exiting.")
+def input ():
+    val = input("Read(r) or write(w)?: ") 
+    if val == "r" :
+        print("Let's read the json data.")
+        read_json_file ()
+        "Thanks for using the pet database."
+    if val == "w" :
+        print("Let's write the json data.")
+        mypet = input("Name of pet: ") 
+        write_json_file ( mypet )
+        "Adding that name."
+        "Thanks for using the pet database."
+    else :
+        print("Please choose read(r) or write(w). Exiting.")
+
+create_reddit_object()
+summon_submission()
